c0ba2e9c993e4b33fbf3ab0bb69d0876d2bd3620
William Breece <kbd1114@gmail.com>
2019-10-14 07:26:30 -0500

    Added surface shader and animation for a sine wave.

--- Assets/Graph/Graph.cs
+++ Assets/Graph/Graph.cs
@@ -4,19 +4,39 @@ public class Graph : MonoBehaviour
 {
     public Transform pointPrefab;
 
+    [Range(10,100)]
+    public int resolution = 10;
+
+    Transform[] points;
+
     private void Awake()
     {
-        Vector3 scale = Vector3.one / 5f;
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one * step;
         Vector3 position;
+        position.y = 0f;
         position.z = 0f;
-        for (int i = 0; i < 10; i++)
+        points = new Transform[resolution];
+        for (int i = 0; i < points.Length; i++)
         {
             Transform point = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) / 5f - 1f;
-            position.y = position.x;
+            position.x = (i + 0.5f) * step - 1f;
             point.localPosition = position;
             point.localScale = scale;
+            point.SetParent(transform, false);
+            points[i] = point;
         }
 
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            point.localPosition = position;
+        }
     }
 }
