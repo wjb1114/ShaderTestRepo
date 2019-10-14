2d8a807b4a948321fd6b865b2de879f4693efc8b
William Breece <kbd1114@gmail.com>
2019-10-14 06:52:22 -0500

    Basic animation working in scene.

--- Assets/Clock/Clock.cs
+++ Assets/Clock/Clock.cs
@@ -1,18 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+using System;
 public class Clock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    const float degreesPerHour = 30f;
+    const float degreesPerMinute = 6f;
+    const float degreesPerSecond = 6f;
 
-    // Update is called once per frame
-    void Update()
+    public Transform hoursTransform;
+    public Transform minutesTransform;
+    public Transform secondsTransform;
+
+    private void Update()
     {
-        
+        DateTime time = DateTime.Now;
+        hoursTransform.localRotation = Quaternion.Euler(0f, time.Hour * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, time.Minute * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, time.Second * degreesPerSecond, 0f);
     }
 }
