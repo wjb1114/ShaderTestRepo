5c34711e7cdb07433201ac42bb451cd3ada7a889
William Breece <kbd1114@gmail.com>
2019-10-14 08:07:55 -0500

    Added z coords to shader and render output, creating wave of cubes in scene.

--- Assets/Graph/Graph.cs
+++ Assets/Graph/Graph.cs
@@ -7,7 +7,10 @@ public class Graph : MonoBehaviour
     [Range(10,100)]
     public int resolution = 10;
 
+    public GraphFunctionName function;
+
     Transform[] points;
+    static GraphFunction[] functions = {SineFunction, MultiSineFunction};
 
     private void Awake()
     {
@@ -16,11 +19,17 @@ public class Graph : MonoBehaviour
         Vector3 position;
         position.y = 0f;
         position.z = 0f;
-        points = new Transform[resolution];
-        for (int i = 0; i < points.Length; i++)
+        points = new Transform[resolution * resolution];
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+            }
             Transform point = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
+            position.x = (x + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
@@ -31,12 +40,27 @@ public class Graph : MonoBehaviour
 
     private void Update()
     {
+        float t = Time.time;
+        GraphFunction f = functions[(int)function];
         for (int i = 0; i < points.Length; i++)
         {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            position.y = f(position.x, position.z, t);
             point.localPosition = position;
         }
+    }
+
+    static float SineFunction (float x, float z, float t)
+    {
+        return Mathf.Sin(Mathf.PI * (x + t));
+    }
+
+    static float MultiSineFunction (float x, float z, float t)
+    {
+        float y = Mathf.Sin(Mathf.PI * (x + t));
+        y += Mathf.Sin(2f * Mathf.PI * (x + 2f * t)) / 2f;
+        y *= 2f / 3f;
+        return y;
     }
 }
