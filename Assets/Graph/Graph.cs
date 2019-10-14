168f39e8e192892611e9300f3a061de1a4bfad8b
William Breece <kbd1114@gmail.com>
2019-10-14 07:11:35 -0500

    Finished clock. Added graph scene, renders linear mathematical function.

---
+++ Assets/Graph/Graph.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    public Transform pointPrefab;
+
+    private void Awake()
+    {
+        Vector3 scale = Vector3.one / 5f;
+        Vector3 position;
+        position.z = 0f;
+        for (int i = 0; i < 10; i++)
+        {
+            Transform point = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) / 5f - 1f;
+            position.y = position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+        }
+
+    }
+}
