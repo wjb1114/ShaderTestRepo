654ef4991cdc093936d6204f84d207059ef12fde
William Breece <kbd1114@gmail.com>
2019-10-15 00:00:14 -0500

    Added Persistence folder and associated ascript, preab and scene. Cubes spawn with keypress, and all cubes are destroyed with another keypress.

---
+++ Assets/Persistence/Game.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Game : MonoBehaviour
+{
+    public Transform prefab;
+
+    public KeyCode createKey = KeyCode.C;
+    public KeyCode newGameKey = KeyCode.N;
+
+    List<Transform> objects;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(createKey))
+        {
+            CreateObject();
+        }
+        else if (Input.GetKeyDown(newGameKey))
+        {
+            BeginNewGame();
+        }
+    }
+    void CreateObject()
+    {
+        Transform t = Instantiate(prefab);
+        t.localPosition = Random.insideUnitSphere * 5f;
+        t.localRotation = Random.rotation;
+        t.localScale = Vector3.one * Random.Range(0.1f, 1f);
+        objects.Add(t);
+    }
+
+    void BeginNewGame()
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            Destroy(objects[i].gameObject);
+        }
+        objects.Clear();
+    }
+
+    private void Awake()
+    {
+        objects = new List<Transform>();
+    }
+}
